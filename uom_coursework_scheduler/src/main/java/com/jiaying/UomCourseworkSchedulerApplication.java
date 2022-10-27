8762a52f3407b70cbe2bea3b20690fa842b32f3c
Jiaying Huang <3395592153@qq.com>
2022-10-27 14:05:33 +0100

    first commit for setup

---
+++ uom_coursework_scheduler/src/main/java/com/jiaying/UomCourseworkSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.jiaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UomCourseworkSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UomCourseworkSchedulerApplication.class, args);
+	}
+
+}
---
+++ uom_coursework_scheduler/src/test/java/com/jiaying/UomCourseworkSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UomCourseworkSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
