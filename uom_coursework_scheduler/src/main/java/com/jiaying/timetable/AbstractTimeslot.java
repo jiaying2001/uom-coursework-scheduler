6ba26daa7cc10d8aba678f2cb08eac93cff1995c
Jiaying Huang <3395592153@qq.com>
2022-11-08 22:46:25 +0000

    Timetable Interface

--- uom_coursework_scheduler/src/main/java/com/jiaying/timetable/AbstractTimeslot.java
+++ uom_coursework_scheduler/src/main/java/com/jiaying/timetable/AbstractTimeslot.java
@@ -1,5 +1,13 @@
 package com.jiaying.timetable;
 
-public class Timeslot implements ITimeslot {
+import java.util.Date;
 
+public abstract class AbstractTimeslot implements ITimeslot {
+		private String shortName;
+		private String fullName;
+		private int duration;
+		private int scheduleDay;
+		private String activityType;
+		private Date startTime;
+		private Date endTime;
 }
--- uom_coursework_scheduler/src/main/java/com/jiaying/timetable/ITimetable.java
+++ uom_coursework_scheduler/src/main/java/com/jiaying/timetable/ITimetable.java
@@ -1,7 +1,20 @@
 package com.jiaying.timetable;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public interface ITimetable {
-	ArrayList<Timeslot> getAvailableSlots();
+	ITimeslot getTimeslotAt(Date date);
+	void moveTimeslotTo(ITimeslot timeslot, Date date);
+	ArrayList<ITimeslot> getAvailableSlots();
+	ArrayList<ITimeslot> getAvailableSlotsAt(Date date);
+	ArrayList<ITimeslot> getAvailableSlotsafter(Date date);
+	ITimeslot getEarliestTimeslot(Date date);
+	ITimeslot getEarliestTimeslotAfter(Date date);
+	ITimeslot getEarliestTimeslotAt(Date date);
+	boolean haveAnyAvailableSlotAt(Date date);
+	boolean haveAnyAvailableSlotAfter(Date date);
+	boolean haveAnyAvailableSlot(Date date);
+	void putTimeslot(ITimeslot timeslot);
+	void removeTimeslot(ITimeslot timeslot);
 }
